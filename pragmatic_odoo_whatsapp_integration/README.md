d652f4efe1aab3c05b8729cacb02ee7d93af6d9d
glnkdev3590 <sysinfo@geolink.com.co>
2021-09-24 14:32:37 -0500

    Pragmatic Wapp integration folder

--- pragmatic_odoo_whatsapp_integration/README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a6752f0e943282dca135dffd769ad2369b98d41d008aa96045f102a0789b28c5
-size 627
--- pragmatic_odoo_whatsapp_integration/__manifest__.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5f6dcdc5feebccace996b63f566bf543f47191ae4f28b088570c97af7907b8c1
-size 4004
